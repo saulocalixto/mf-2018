49af9e5bec8757b3ea77d6dea2afb4b922f3e4db
Saulo Calixto <sauloacalixto@gmail.com>
2018-09-03 21:46:08 -0300

    Acrescentando arquivo semana

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,33 @@
+package br.ufg.inf.mf.pratica;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Aplicacao {
+	public static void main(String args[]) {
+
+		System.out.println(LocalDate.now().getDayOfWeek());
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
