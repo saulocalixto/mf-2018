389062220a669883b089276f9b521f976ffc945a
Saulo Calixto <sauloacalixto@gmail.com>
2018-09-16 09:48:13 -0300

    Acrescentando comentários à classe Nome

--- aula-3/semana/br/ufg/inf/mf/pratica/Nome.java
+++ aula-3/semana/br/ufg/inf/mf/pratica/Nome.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2016. Engenharia de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
 package br.ufg.inf.mf.pratica;
 
 import java.util.Locale; 
@@ -5,7 +9,19 @@ import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+*
+* @author Saulo A. Calixto
+*
+*/
 public class Nome {
+
+       /**
+        *
+        * Método principal da classe Nome que apenas imprime o dia da semana
+	* baseado na configuração da máquina do usuário.
+        *
+        */
 	public static void main(String args[]) {
 		Locale linguaCorrente = Locale.getDefault();
 		Calendar calendario = Calendar.getInstance();
